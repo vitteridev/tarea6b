50d5d7e57d19a19f86ff3a578b487320542a7916
Junior Vitteri <vitteridev@gmail.com>
2021-12-09 04:23:50 -0300

    agrega estilos con bootstrap

--- tarea6.js
+++ tarea6.js
@@ -1,13 +1,26 @@
 function ocultarElemento(elemento) {
-  elemento.className = "oculto";
+  elemento.classList.add("oculto");
 }
 function mostrarElemento(elemento) {
-  elemento.className = "";
+  elemento.classList.remove("oculto");
+}
+
+function deshabilitarBotonesAgregarYEliminar() {
+  document.querySelector("#agregar-integrante").disabled = true;
+  document.querySelector("#eliminar-integrante").disabled = true;
+}
+
+function habilitarBotonesAgregarYEliminar() {
+  document.querySelector("#agregar-integrante").disabled = false;
+  document.querySelector("#eliminar-integrante").disabled = false;
 }
 
 function resetear() {
   const $analisis = document.querySelector("#analisis");
   const $btnResetear = document.querySelector("#resetear");
+
+  habilitarBotonesAgregarYEliminar();
+
   ocultarElemento($analisis);
   ocultarElemento($btnResetear);
 }
@@ -31,12 +44,16 @@ function mostrarSalario(tipo, valor) {
 function crearIntegrante() {
   const div = document.createElement("div");
   div.className = "integrante";
+  div.classList.add("mb-2");
+  div.classList.add("mt-2");
 
   const label = document.createElement("label");
   label.textContent = "ingrese salario anual del integrante : ";
 
   const input = document.createElement("input");
   input.type = "number";
+  input.classList.add("form-control");
+  input.classList.add("form-control-sm");
 
   div.appendChild(label);
   div.appendChild(input);
@@ -77,6 +94,8 @@ document.querySelector("#calcular").onclick = function (event) {
   const salarios = obtenerSalarioIntegrantes();
   const $btnResetear = document.querySelector("#resetear");
   const $analisis = document.querySelector("#analisis");
+
+  deshabilitarBotonesAgregarYEliminar();
 
   mostrarSalario("mayor", obtenerMayorSalarioAnual(salarios));
   mostrarSalario("menor", obtenerMenorSalarioAnual(salarios));
